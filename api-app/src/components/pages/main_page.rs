05b5d7e260e9b1097b231e7e33ad30be59fa8059
CutoNaito <loliknightowo@gmail.com>
2023-03-09 22:51:09 +0100

    Added sass

--- api-app/src/components/pages/main_page.rs
+++ api-app/src/components/pages/main_page.rs
@@ -2,6 +2,15 @@ use yew::{function_component, html, Html};
 use crate::components::header::Header;
 use crate::components::footer::Footer;
 
+#[function_component(SideMenu)]
+pub fn side_menu() -> Html {
+    html! {
+        <div class="side-menu">
+            <p>{"There is nothing yet, WIP"}</p>
+        </div>
+    }
+}
+
 #[function_component(MainPage)]
 pub fn main_page() -> Html {
     html! {
@@ -9,7 +18,7 @@ pub fn main_page() -> Html {
             <Header />
             <main>
                 <div class="container">
-                    
+                    <SideMenu />
                 </div>
             </main>
             <Footer />
