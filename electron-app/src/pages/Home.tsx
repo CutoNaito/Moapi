1b680181f1fc6f9edcff4770f6acd88629b492e6
CutoNaito <loliknightowo@gmail.com>
2023-03-22 18:03:24 +0100

    Added request history storing

--- electron-app/src/pages/Home.tsx
+++ electron-app/src/pages/Home.tsx
@@ -3,13 +3,29 @@ import { useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
 import { Footer } from "../components/Footer";
 import JsonFormatter from "react-json-formatter";
+import 'dotenv/config';
+
+if (!process.env.SERVER_URI) {
+    throw new Error("SERVER_URI not set");
+}
+
+const SERVER_URI = process.env.SERVER_URI;
 
 export function Home() {
     const [url, setUrl] = useState("");
+    const [user_id, setUser_id] = useState("");
     const [method, setMethod] = useState("");
     const [body, setBody] = useState("");
     const [bodyParsed, setBodyParsed] = useState({});
     const [response, setResponse] = useState({});
+
+    async function fetchUsername() {
+        const token = document.cookie.split("=")[1];
+        const response = await fetch(SERVER_URI + "/users/token/" + token);
+        const data = await response.json();
+        
+        setUser_id(data[0].ID);
+    }
 
     const history = useNavigate();
 
@@ -23,7 +39,7 @@ export function Home() {
         if (!document.cookie.includes("token")) {
             history("/login");
         } else {
-            console.log("You are logged in");
+            fetchUsername();
         }
     }, []);
 
@@ -33,6 +49,7 @@ export function Home() {
         if (method == "POST" || method == "PUT") {
             try {
                 setBodyParsed(JSON.parse(body));
+                
                 const response = await fetch(url, {
                     method: method,
                     headers: {
@@ -45,7 +62,19 @@ export function Home() {
                     setResponse({"error": response.statusText});
                 } else {
                     const data = await response.json();
+
                     setResponse(data);
+
+                    await fetch(SERVER_URI + "/stored_uris", {
+                        method: "POST",
+                        headers: {
+                            "Content-Type": "application/json"
+                        },
+                        body: JSON.stringify({
+                            uri: url,
+                            user_id: user_id
+                        })
+                    });
                 }
             } catch (error) {
                 setResponse({"error": error});
