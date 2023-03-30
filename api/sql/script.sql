80a842a367de3707201cea64a98b629f887af7a2
CutoNaito <loliknightowo@gmail.com>
2023-03-30 17:17:23 +0200

    Added an SQL script

---
+++ api/sql/script.sql
@@ -0,0 +1,28 @@
+CREATE DATABASE api_app;
+USE api_app;
+
+CREATE TABLE `users`(
+	`ID` CHAR(36) PRIMARY KEY NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `email` VARCHAR(100) NOT NULL,
+    `password` VARCHAR(255) NOT NULL,
+    `token` VARCHAR(255) NOT NULL,
+    `verified` BOOLEAN NOT NULL,
+    `verification_code` VARCHAR(255)
+);
+
+CREATE TABLE `stored_URIs`(
+	`ID` CHAR(36) PRIMARY KEY NOT NULL,
+    `ID_users` CHAR(36) NOT NULL,
+    CONSTRAINT `ID_users_constr` FOREIGN KEY(`ID_users`) REFERENCES `users`(`ID`),
+    `URI` VARCHAR(255) NOT NULL,
+    `method` ENUM('GET', 'POST', 'PUT', 'DELETE')
+);
+
+CREATE TABLE `favorites`(
+	`ID` CHAR(36) PRIMARY KEY NOT NULL,
+    `ID_users` CHAR(36) NOT NULL,
+    CONSTRAINT `ID_users_fav_constr` FOREIGN KEY(`ID_users`) REFERENCES `users`(`ID`),
+    `URI` VARCHAR(255) NOT NULL,
+    `method` ENUM('GET', 'POST', 'PUT', 'DELETE')
+);
