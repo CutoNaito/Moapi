365077cd20ab9a837683eb537b76e192c9a70d0f
CutoNaito <loliknightowo@gmail.com>
2023-04-08 13:27:45 +0200

    userpage css update

--- web/src/pages/Userpage.tsx
+++ web/src/pages/Userpage.tsx
@@ -27,51 +27,86 @@ export function Userpage() {
 
     const [username, setUsername] = useState("");
     const [requestHistory, setRequestHistory] = useState<string[]>([]);
+    const [favoriteHistory, setFavoriteHistory] = useState<string[]>([]);
+
+    const fetchUser = async () => {
+        const response = await fetch(SERVER_URI + `/users/${userID}`);
+        const data = await response.json();
+
+        if (data.error) {
+            history("/");
+        };
+
+        setUsername(data.username);
+    };
+
+    const fetchRequestHistory = async () => {
+        const response = await fetch(SERVER_URI + `/stored_uris/user/${userID}`);
+        const data = await response.json();
+
+        console.log(data);
+        
+        if (data.error) {
+            setRequestHistory([]);
+        };
+
+        const URIs = data.map((x: Stored_URIs) => {
+            return x.URI;
+        });
+
+        setRequestHistory(URIs);
+    };
+
+    const fetchFavoriteHistory = async () => {
+        const response = await fetch(SERVER_URI + `/favorites/user/${userID}`);
+        const data = await response.json();
+
+        if (data.error) {
+            setFavoriteHistory([]);
+        };
+
+        const URIs = data.map((x: Stored_URIs) => {
+            return x.URI;
+        });
+
+        setFavoriteHistory(URIs);
+    };
 
     useEffect(() => {
-        const fetchUser = async () => {
-            const response = await fetch(SERVER_URI + `/users/${userID}`);
-            const data = await response.json();
-
-            if (data.error) {
-                history("/");
-            };
-
-            setUsername(data.username);
-        };
-
-        const fetchRequestHistory = async () => {
-            const response = await fetch(SERVER_URI + `/stored_uris/user/${userID}`);
-            const data = await response.json();
-
-            console.log(data);
-            
-            if (data.error) {
-                setRequestHistory([]);
-            };
-
-            const URIs = data.map((x: Stored_URIs) => {
-                return x.URI;
-            });
-
-            setRequestHistory(URIs);
-        };
-
         fetchUser();
         fetchRequestHistory();
+        fetchFavoriteHistory();
     }, []);
 
     return (
-        <div className="userpage">
+        <div>
             <Header />
-            <h1>{username}</h1>
-            <ul>
-                {requestHistory.map((x) =>
-                    <li>
-                        {x}
-                    </li>
-                )}
-            </ul>
+            <div className="userpage">
+                <h1>{username}</h1>
+
+                <div className="navbars">
+                    <nav className="history-navbar">
+                        <ul>
+                            {requestHistory.map((x) =>
+                                <li>
+                                    {x}
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+
+                    <nav className="favorite-navbar">
+                        <ul>
+                            {favoriteHistory.map((x) =>
+                                <li>
+                                    {x}
+                                </li>
+                            )}
+                        </ul>
+                    </nav>
+                </div>
+                
+            </div>
             <Footer />
         </div>
     );
